b4d3902f601164f0ce4bd5a349990e4476783533
chentobiana <chentobiana@gmail.com>
2024-11-05 12:49:16 +0200

    change App

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { 
+  Box,
+  Flex,
+} from "monday-ui-react-core";
 import ItemTable from './components/ItemTable';
 import "monday-ui-react-core/dist/main.css";
 import styles from './App.module.css';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1
+    },
+  },
+});
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className={styles.appContainer}>
-        <div className={styles.contentWrapper}>
-          <h1 className={styles.mainTitle}>Monday Board Items</h1>
+      <Box className={styles.appContainer}>
+        <Box className={styles.contentWrapper}>
+          <Flex 
+            className={styles.header}
+            justify={Flex.justify.START} 
+            align={Flex.align.CENTER}
+          >
+            <h1>Monday Board Items</h1>
+          </Flex>
           <ItemTable />
-        </div>
-      </div>
+        </Box>
+      </Box>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
